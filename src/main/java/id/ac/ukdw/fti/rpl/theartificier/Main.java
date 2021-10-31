705f3284a7286403559ab1bd7f6b95d3a96e9fcb
nathanaelptm <89833174+nathanaelptm@users.noreply.github.com>
2021-10-31 20:44:02 +0700

    Ikon Aplikasi pojokan

--- src/main/java/id/ac/ukdw/fti/rpl/theartificier/Main.java
+++ src/main/java/id/ac/ukdw/fti/rpl/theartificier/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package id.ac.ukdw.fti.rpl.theartificier;
 
 import java.io.File;
@@ -32,3 +33,40 @@ public class Main extends Application {
     }
     
 }
+=======
+package id.ac.ukdw.fti.rpl.theartificier;
+
+import java.io.File;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+       
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        // stage.getIcons().add(new Image(new File("appicon.png").toURI().toString()));
+        stage.setTitle("New Jerusalem");
+        Scene scene = new Scene(root);
+        // stage.getIcons().add(new Image("/img/appicon.png"));
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("/img/appicon.png")));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
+>>>>>>> Stashed changes
